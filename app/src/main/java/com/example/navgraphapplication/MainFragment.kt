6c4a253a1377fd86bff5e9340bcbe8eb54f7820a
Aleksandra Krzemien <aleksandra.a.krzemien@gmail.com>
2022-07-08 11:05:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navgraphapplication/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.example.navgraphapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
