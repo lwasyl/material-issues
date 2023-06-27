c6120d6d3d9e40040db81bf2a307ac0abcb777f2
Łukasz Wasylkowski <l.wasyl@gmail.com>
2023-06-27 15:51:32 +0200

    wip

--- app/src/main/java/com/example/navgraphapplication/FirstFragment.kt
+++ app/src/main/java/com/example/navgraphapplication/FirstFragment.kt
@@ -6,9 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
@@ -30,27 +28,16 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button1).setOnClickListener {
-            navController.navigate(Uri.parse("https://www.example.com/second/"))
+            // navController.navigate(Uri.parse("https://www.example.com/second/"))
+
+            Snackbar.make(view.findViewById(R.id.container), "text", Snackbar.LENGTH_INDEFINITE)
+                .apply {
+                    anchorView = view.findViewById(R.id.anchor)
+                    isAnchorViewLayoutListenerEnabled = true
+
+                    show()
+                }
         }
 
-        snackbarExample(view)
-    }
-
-    private fun snackbarExample(view: View) {
-        showSnackbar(view.findViewById(R.id.container), view.findViewById(R.id.anchorButton))
-
-        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
-            view.findViewById<Button>(R.id.anchorButton).isVisible = true
-        }
-    }
-
-    private fun showSnackbar(container: View, bottomAnchor: View) {
-        Snackbar.make(container, "text", Snackbar.LENGTH_INDEFINITE)
-            .apply {
-                anchorView = bottomAnchor
-                isAnchorViewLayoutListenerEnabled = true
-
-                show()
-            }
     }
 }
